9218a4f03ec204dbb2a7bc363dfb479fd3467fa4
hibiki <20015683+hibikidesu@users.noreply.github.com>
2020-12-22 01:17:30 +0800

    querytexture

--- data/main.lua
+++ data/main.lua
@@ -1,17 +1,12 @@
 require "menu"
 
-button = {};
-button.tex = image.load("assets/system/ui/button.png");
-button.w, button.h = render.queryTexture(button.tex);
-
 function main_loop()
-	if controls.mouse.down then
-		print("X", controls.mouse.x)
-		print("Y", controls.mouse.y)
+	local scene = engine.getScene();
+	if scene == 0 then
+		menu_render();
 	end
-	render.showTexture(button.tex, 50, 0, button.w, button.h);
 end
 
 function shutdown() 
-	image.free(button.tex) 
+	menu_shutdown();
 end
--- data/menu.lua
+++ data/menu.lua
@@ -1,3 +1,11 @@
+button = {};
+button.tex = image.load("assets/system/ui/button.png");
+button.w, button.h = render.queryTexture(button.tex);
+
 function menu_render()
-    
+    render.showTexture(button.tex, 50, 0, button.w, button.h);
+end
+
+function menu_shutdown() 
+    image.free(button.tex)
 end
